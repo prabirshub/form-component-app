7645891ba5848155d2d23ad8a2466df1b442cad4
Prabir Singh <prabirsinghdigital@gmail.com>
2022-12-06 09:52:04 +0530

    practicing form components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import './App.css'
 
 function App() {
+  const [name, setName] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setName('')
+    console.log('Form submitted')
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <h1>Form Components</h1>
+        <form onSubmit={handleSubmit}>
+          <fieldset>
+            <div className='field'>
+              <label htmlFor='name'> Name </label>
+              <input
+                type='text'
+                id='name'
+                placeholder='Name'
+                name='name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                className='app-input'
+              />
+            </div>
+            <button className='btn' disabled={!name} type='submit'>
+              Submit
+            </button>
+          </fieldset>
+        </form>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
